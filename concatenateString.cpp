762a294b82a13fbbbce090443d6fc170fb506cbd
ruhanika06 <ruhanika.thatai@gmail.com>
2025-08-17 20:34:45 +0530

    Create concatenateString.cpp

---
+++ concatenateString.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str1, str2;
+
+    cout << "Enter first string: ";
+    getline(cin, str1);
+
+    cout << "Enter second string: ";
+    getline(cin, str2);
+
+    // Concatenate str2 to str1
+    str1 = str1 + str2;
+
+    cout << "Concatenated string: " << str1 << endl;
+
+    return 0;
+}
