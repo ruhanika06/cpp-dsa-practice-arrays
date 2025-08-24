9378f068b1904de8d2ad9eb340c55d11b472b3cf
ruhanika06 <ruhanika.thatai@gmail.com>
2025-08-24 11:23:38 +0530

    Create distinct_elements.cpp

---
+++ distinct_elements.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int countDistinct(int arr[], int n) {
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        bool isDistinct = true;
+
+        // Check if arr[i] has appeared before
+        for (int j = 0; j < i; j++) {
+            if (arr[i] == arr[j]) {
+                isDistinct = false;
+                break;
+            }
+        }
+
+        if (isDistinct) count++;
+    }
+
+    return count;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << "Total distinct elements = " << countDistinct(arr, n) << endl;
+
+    return 0;
+}
