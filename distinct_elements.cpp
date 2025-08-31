d95cb5e1e3c545b80b2bad9a5d214912aea8c7d3
ruhanika06 <ruhanika.thatai@gmail.com>
2025-08-31 15:06:51 +0530

    Update distinct_elements.cpp

--- distinct_elements.cpp
+++ distinct_elements.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int countDistinct(int arr[], int n) {
+int countDistinct(int arr[], int n) {//o(n^2)
     int count = 0;
 
     for (int i = 0; i < n; i++) {
@@ -29,6 +29,15 @@ int main() {
     int arr[n];
     cout << "Enter elements: ";
     for (int i = 0; i < n; i++) cin >> arr[i];
+
+    unordered_set<int> s;  // Declare unordered_set
+
+    // Insert all elements into the set
+    for(int i=0; i<n; i++){//better approach -o(n)
+        s.insert(arr[i]);  
+    }
+
+    cout << "Total distinct elements: " << s.size() << endl;
 
     cout << "Total distinct elements = " << countDistinct(arr, n) << endl;
 
